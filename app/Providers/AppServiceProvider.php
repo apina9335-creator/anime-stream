1feb153e16ba1999a9ee85a119cb04971dc110fc
apina9335-creator <apina9335@gmail.com>
2026-02-09 00:05:20 +0700

    fix broken css force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- 1. WAJIB ADA INI BIAR GAK ERROR
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // <--- 2. LOGIKA PAKSA HTTPS (KHUSUS RAILWAY)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
